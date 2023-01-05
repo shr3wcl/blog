474856fc2d0ea143f2d4a3bf472d0ed5cc4bd760
Bin-08-01 <minhtri192035@gmail.com>
2023-01-06 00:01:51 +0700

    Update NotionRenderer and CSS

--- components/layout/postView.tsx
+++ components/layout/postView.tsx
@@ -1,18 +1,110 @@
 import {Post} from "../notion/postType";
 import {NotionRenderer} from "react-notion-x";
+import dynamic from "next/dynamic";
+import Link from "next/link";
+import Image from "next/image";
+
+
+const Code = dynamic(async () => {
+    const m = await import("react-notion-x/build/third-party/code");
+    return m.Code;
+});
+
+const Modal = dynamic(
+    () => import("react-notion-x/build/third-party/modal").then((m) => m.Modal),
+    {
+        ssr: false,
+    }
+);
+
+// const TweetRender = ({ id }: { id: string }) => {
+//     return <TweetEmbed tweetId={id} />;
+// };
 
 export const PostView = ({post}: { post: Post }) => {
+
     return (
         <div className={"w-full dark:bg-[#171717] dark:text-gray-50 "}>
-            <div className={""}>
-                <NotionRenderer recordMap={post.recordMap} fullPage={true} darkMode={false} showTableOfContents
+            <article>
+                <NotionRenderer components={{
+                    Code,
+                    // collection: Collection,
+                    // collectionRow: CollectionRow,
+                    nextImage: Image,
+                    nextLink: Link,
+                    // Tweet: TweetRender,
+                    Modal,
+                }} recordMap={post.recordMap} fullPage={true} darkMode={false} showTableOfContents
                                 minTableOfContentsItems={3} className={""}/>
-            </div>
+            </article>
             <style global jsx>{`
+
+              ul {
+                list-style-type: disc !important;
+                list-style-position: inside !important;
+              }
+
+              ol {
+                list-style-type: decimal !important;
+                list-style-position: inside !important;
+              }
+
+              blockquote {
+                border-left: 3px solid black !important;
+              }
+
+              h1, h2, h3, h4, h5, h6 {
+                padding: 20px 0 !important;
+              }
+              
+              h1{
+                font-size: 2em;
+                margin-top: 1.1em;
+              }
+              
+              h2{
+                font-size: 1.75em;
+                margin-top: 1.1em;
+              }
+              
+              h3 {
+                font-size: 1.5em;
+                margin-top: 1.1em;
+                position: relative;
+                display: inline-block;
+                font-weight: 600;
+                line-height: 1.3;
+                padding: 3px 2px;
+                margin-bottom: 1px;
+                max-width: 100%;
+                white-space: pre-wrap;
+                word-break: break-word;
+              }
+              
+              h4{
+                font-size: 1.25em;
+                margin-top: 1.1em;
+              }
+              
+              h5{
+              font-size: 1em;
+                margin-top: 1.1em;
+              }
+              
+              h6{
+              font-size: 0.75em;
+                margin-top: 1.1em;
+              }
+
+              blockquote > * {
+                padding: 4px 20px;
+              }
+
               .notion-page {
                 width: var(--notion-max-width);
                 padding-left: calc(min(16px, 8vw));
                 padding-right: calc(min(16px, 8vw));
+                max-width: 60%;
               }
 
               .note-page-content * {
@@ -30,19 +122,21 @@ export const PostView = ({post}: { post: Post }) => {
                 flex-direction: column;
                 width: 100%;
                 height: 100%;
-                margin-top: 40px;
+                margin-top: 20px;
+
               }
 
-              .notion-asset-wrapper, .notion-asset-wrapper-video{
+              .notion-asset-wrapper, .notion-asset-wrapper-video {
                 display: flex;
                 flex-direction: column;
                 justify-content: center;
                 align-items: center;
+                margin: 6px 0;
               }
 
               .notion-header {
                 position: sticky;
-                top: 3.4rem;
+                top: 1rem;
                 left: 0;
                 z-index: 200;
                 width: 100%;
@@ -131,48 +225,48 @@ export const PostView = ({post}: { post: Post }) => {
                 margin-left: 16px;
               }
 
-              //.dark .notion {
-              //  --fg-color: rgba(255, 255, 255, 0.9);
-              //  --fg-color-0: var(--fg-color);
-              //  --fg-color-1: var(--fg-color);
-              //  --fg-color-2: var(--fg-color);
-              //  --fg-color-3: var(--fg-color);
-              //  --fg-color-4: var(--fg-color);
-              //  --fg-color-5: rgba(255, 255, 255, 0.7);
-              //  --fg-color-6: #fff;
-              //  --fg-color-icon: #fff;
-              //  --bg-color: #18181b;
-              //  --bg-color-0: rgb(71, 76, 80);
-              //  --bg-color-1: rgb(63, 68, 71);
-              //  --bg-color-2: rgba(135, 131, 120, 0.15);
-              //  --notion-red: rgb(255, 115, 105);
-              //  --notion-pink: rgb(226, 85, 161);
-              //  --notion-blue: rgb(82, 156, 202);
-              //  --notion-purple: rgb(154, 109, 215);
-              //  --notion-teal: rgb(77, 171, 154);
-              //  --notion-yellow: rgb(255, 220, 73);
-              //  --notion-orange: rgb(255, 163, 68);
-              //  --notion-brown: rgb(147, 114, 100);
-              //  --notion-gray: rgba(151, 154, 155, 0.95);
-              //  --notion-red_background: rgb(89, 65, 65);
-              //  --notion-pink_background: rgb(83, 59, 76);
-              //  --notion-blue_background: rgb(54, 73, 84);
-              //  --notion-purple_background: rgb(68, 63, 87);
-              //  --notion-teal_background: rgb(53, 76, 75);
-              //  --notion-yellow_background: rgb(89, 86, 59);
-              //  --notion-orange_background: rgb(89, 74, 58);
-              //  --notion-brown_background: rgb(67, 64, 64);
-              //  --notion-gray_background: rgb(69, 75, 78);
-              //  --notion-red_background_co: rgba(89, 65, 65, 0.3);
-              //  --notion-pink_background_co: rgba(83, 59, 76, 0.3);
-              //  --notion-blue_background_co: rgba(120, 162, 187, 0.3);
-              //  --notion-purple_background_co: rgba(68, 63, 87, 0.3);
-              //  --notion-teal_background_co: rgba(53, 76, 75, 0.3);
-              //  --notion-yellow_background_co: rgba(89, 86, 59, 0.3);
-              //  --notion-orange_background_co: rgba(89, 74, 58, 0.3);
-              //  --notion-brown_background_co: rgba(67, 64, 64, 0.3);
-              //  --notion-gray_background_co: rgba(69, 75, 78, 0.3);
-              //}
+              .dark .notion {
+                --fg-color: rgba(255, 255, 255, 0.9);
+                --fg-color-0: var(--fg-color);
+                --fg-color-1: var(--fg-color);
+                --fg-color-2: var(--fg-color);
+                --fg-color-3: var(--fg-color);
+                --fg-color-4: var(--fg-color);
+                --fg-color-5: rgba(255, 255, 255, 0.7);
+                --fg-color-6: #fff;
+                --fg-color-icon: #fff;
+                --bg-color: #18181b;
+                --bg-color-0: rgb(71, 76, 80);
+                --bg-color-1: rgb(63, 68, 71);
+                --bg-color-2: rgba(135, 131, 120, 0.15);
+                --notion-red: rgb(255, 115, 105);
+                --notion-pink: rgb(226, 85, 161);
+                --notion-blue: rgb(82, 156, 202);
+                --notion-purple: rgb(154, 109, 215);
+                --notion-teal: rgb(77, 171, 154);
+                --notion-yellow: rgb(255, 220, 73);
+                --notion-orange: rgb(255, 163, 68);
+                --notion-brown: rgb(147, 114, 100);
+                --notion-gray: rgba(151, 154, 155, 0.95);
+                --notion-red_background: rgb(89, 65, 65);
+                --notion-pink_background: rgb(83, 59, 76);
+                --notion-blue_background: rgb(54, 73, 84);
+                --notion-purple_background: rgb(68, 63, 87);
+                --notion-teal_background: rgb(53, 76, 75);
+                --notion-yellow_background: rgb(89, 86, 59);
+                --notion-orange_background: rgb(89, 74, 58);
+                --notion-brown_background: rgb(67, 64, 64);
+                --notion-gray_background: rgb(69, 75, 78);
+                --notion-red_background_co: rgba(89, 65, 65, 0.3);
+                --notion-pink_background_co: rgba(83, 59, 76, 0.3);
+                --notion-blue_background_co: rgba(120, 162, 187, 0.3);
+                --notion-purple_background_co: rgba(68, 63, 87, 0.3);
+                --notion-teal_background_co: rgba(53, 76, 75, 0.3);
+                --notion-yellow_background_co: rgba(89, 86, 59, 0.3);
+                --notion-orange_background_co: rgba(89, 74, 58, 0.3);
+                --notion-brown_background_co: rgba(67, 64, 64, 0.3);
+                --notion-gray_background_co: rgba(69, 75, 78, 0.3);
+              }
             `}</style>
         </div>
     )
