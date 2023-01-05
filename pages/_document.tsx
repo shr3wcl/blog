841739afbbc40e0cb66fcac84abd7526f70fb004
Bin-08-01 <minhtri192035@gmail.com>
2023-01-06 00:02:10 +0700

    Update Style CSS

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" className="scroll-smooth">
       <Head />
-      <body>
+      <body className="antialiased bg-white dark:text-gray-50 dark:bg-gray-900">
         <Main />
         <NextScript />
       </body>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
-/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  darkMode: "class",
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class", // or 'media' or 'class'
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/line-clamp")],
   theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    extend: {
+      colors: {
+        gray: colors.neutral,
+      }
+    },
+  }
+};
