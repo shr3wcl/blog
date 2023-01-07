ad1f4457eb330e80003db3bd4c1574eb19318d8e
Bin-08-01 <minhtri192035@gmail.com>
2023-01-08 02:44:58 +0700

    .env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NOTION_KEY: process.env.NOTION_KEY,
+    NOTION_BLOG_ID: process.env.NOTION_BLOG_ID
+  }
 }
 
 module.exports = nextConfig
