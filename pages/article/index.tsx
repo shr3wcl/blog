33618bec4ecb4e0243b9feb368bb724b3d51fcbc
Bin-08-01 <minhtri192035@gmail.com>
2023-01-18 02:58:35 +0700

    Update, useSWR instead of getStaticProps

--- pages/article/index.tsx
+++ pages/article/index.tsx
@@ -1,29 +1,44 @@
 import Head from "next/head";
 import ArticleList from "../../components/article";
+import useSWR, { Key, Fetcher } from 'swr'
+import fetch from "node-fetch";
+import axios from "axios";
 
-export const getStaticProps = async () => {
-    const res = await fetch("https://api.raindrop.io/rest/v1/raindrops/30340862",
-        {
-            headers: {
-                Authorization: `Bearer ${process.env.RAINDROP_TOKEN}`
-            },
-        });
-    const data = await res.json();
+// export const getStaticProps = async () => {
+//     const res = await fetch("https://api.raindrop.io/rest/v1/raindrops/30340862",
+//         {
+//             headers: {
+//                 Authorization: `Bearer ${process.env.RAINDROP_TOKEN}`
+//             },
+//         });
+//     const data = await res.json();
+//     return {
+//         props: {
+//             articleList: data.items,
+//             revalidate: 10
+//         }
+//     }
+// }
+const fetcher = (url: string, token: any) => axios.get(url, {headers: { Authorization: `Bearer ${token}`}}).then(res => res.data).catch(err => console.log(err));;
+const useArticle = () => {
+    const {data, error, isLoading} = useSWR([`https://api.raindrop.io/rest/v1/raindrops/30340862`, process.env.RAINDROP_TOKEN], ([url, token]) => fetcher(url, token));
     return {
-        props: {
-            articleList: data.items,
-            revalidate: 10
-        }
+        article: data,
+        isLoading,
+        isError: error
     }
 }
 
-const Article = ({articleList}: any) => {
+const Article = () => {
+    const {article, isLoading, isError} = useArticle();
+    if (isLoading) return console.log(1);
+    if (isError) return console.log(2);
     return (
         <div>
             <Head>
                 <title>Article Saved</title>
             </Head>
-            <ArticleList articleList={articleList}/>
+            <ArticleList articleList={article.items}/>
         </div>
     )
 }
