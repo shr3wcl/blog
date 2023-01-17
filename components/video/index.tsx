8ce1d2587c9277d57b2a7f499aac1239c9f0d1ba
Bin-08-01 <minhtri192035@gmail.com>
2023-01-18 03:55:01 +0700

    Use swr

--- components/video/index.tsx
+++ components/video/index.tsx
@@ -5,24 +5,31 @@ import {AiOutlineYoutube} from "react-icons/ai";
 import Image from "next/image";
 
 const ListVideo = ({videoList}: any) => {
-    return(
+    return (
         <div className={"grid grid-cols-1 gap-4 sm:grid-cols-2"}>
             {videoList.map((each: any) => {
-                return (
-                    <div key={each.id.videoId}
-                         className=" dark:bg-[#111827] my-1 border-[1px] border-gray-300 hover:border-blue-400 dark:hover:border-blue-400 dark:border-gray-700 dark:bg-gray-800 w-full">
-                        <Link href={`https://www.youtube.com/watch?v=${each.id.videoId}`} target={"_blank"} className={"flex items-center"}>
-                            <div className={"mr-3"}>
-                                <img src={each.snippet.thumbnails.medium.url} alt="Thumbnails" className={"w-52 p-1 rounded-xl"}/>
-                            </div>
-                            <div className={"flex-col my-3"}>
-                                <p className={"mb-2 dark:text-blue-400 font-semibold flex"}><AiOutlineYoutube className={"relative top-[5px] mr-1"}/>{each.snippet.title}</p>
-                                <p className={"text-sm text-gray-500 mb-1"}>{each.snippet.description}</p>
-                                <p className={"text-sm text-gray-500 flex"}><BiTime className={"relative top-1 mr-1"}/>{dayjs(each.snippet.publishedAt, "MM-DD-YYYY").format("hh:mm | MMM DD, YYYY")}</p>
-                            </div>
-                        </Link>
-                    </div>
-                )
+                if (each.id.kind === "youtube#video") {
+                    return (
+                        <div key={each.id.videoId}
+                             className=" dark:bg-[#111827] my-1 border-[1px] border-gray-300 hover:border-blue-400 dark:hover:border-blue-400 dark:border-gray-700 dark:bg-gray-800 w-full">
+                            <Link href={`https://www.youtube.com/watch?v=${each.id.videoId}`} target={"_blank"}
+                                  className={"flex items-center"}>
+                                <div className={"mr-3"}>
+                                    <img src={each.snippet.thumbnails.medium.url} alt="Thumbnails"
+                                         className={"w-52 p-1 rounded-xl"}/>
+                                </div>
+                                <div className={"flex-col my-3"}>
+                                    <p className={"mb-2 dark:text-blue-400 font-semibold flex"}><AiOutlineYoutube
+                                        className={"relative top-[5px] mr-1"}/>{each.snippet.title}</p>
+                                    <p className={"text-sm text-gray-500 mb-1"}>{each.snippet.description}</p>
+                                    <p className={"text-sm text-gray-500 flex"}><BiTime
+                                        className={"relative top-1 mr-1"}/>{dayjs(each.snippet.publishedAt, "MM-DD-YYYY").format("hh:mm | MMM DD, YYYY")}
+                                    </p>
+                                </div>
+                            </Link>
+                        </div>
+                    )
+                }
             })}
         </div>
     )
--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,18 +1,25 @@
 import ProjectList from "../../components/projects";
 import Head from "next/head";
+import useSWR from "swr";
+import axios from "axios";
+import Loading from "../../components/loading";
+import ErrorPage from "../../components/error";
 
-export const getStaticProps = async () => {
-    const res = await fetch("https://api.github.com/users/Bin-08-01/repos");
-    const data = await res.json();
+const fetcher = (url: string, token: any) => axios.get(url).then(res => res.data).catch(err => console.log(err));
+
+const useProjects = () => {
+    const {data, error, isLoading} = useSWR("https://api.github.com/users/Bin-08-01/repos", fetcher);
     return {
-        props: {
-            projects: data,
-            revalidate: 60 * 60
-        }
+        projects: data,
+        isLoading,
+        isError: error
     }
 }
 
-const Project = ({projects}: any) => {
+const Project = () => {
+    const {projects, isLoading, isError} = useProjects();
+    if(isLoading) return <Loading/>;
+    if(isError) return <ErrorPage/>
     return (
         <div className={"dark:bg-[#171717] dark:text-gray-50 min-h-screen w-full"}>
             <Head>
