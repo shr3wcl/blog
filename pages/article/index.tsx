91c6cd01f1ba85e29e88af506d617382c8afd9bb
Bin-08-01 <minhtri192035@gmail.com>
2023-03-07 23:19:23 +0700

    Update index.tsx

--- pages/article/index.tsx
+++ pages/article/index.tsx
@@ -27,8 +27,13 @@ import {useInfiniteQuery} from "@tanstack/react-query";
 const fetcher = (url: string, token: any) => axios.get(url, {headers: { Authorization: `Bearer ${token}`}}).then(res => res.data).catch(err => console.log(err));
 const useArticle = (page: number, search: string) => {
     const {data, error, isLoading} = useSWR([`https://api.raindrop.io/rest/v1/raindrops/30340862?sort=-created&perpage=10&page=${page}&search=${search}`, process.env.RAINDROP_TOKEN], ([url, token]) => fetcher(url, token));
+    const dataTags = useSWR([`https://api.raindrop.io/rest/v1/tags/30340862`, process.env.RAINDROP_TOKEN], ([url, token]) => fetcher(url, token));
+    const tags  = dataTags.data;
     return {
         article: data,
+        tags,
+        isLoadingTag: dataTags.isLoading,
+        isErrorTag: dataTags.error,
         isLoading,
         isError: error
     }
@@ -39,8 +44,10 @@ const Article = () => {
     let [page, setPage] = useState(0);
     let [keySearch, setKeySearch] = useState(router.query.search?.at(0) || '');
     const searchInputRef = useRef<HTMLInputElement>(null);
+    const [hashtagCheck, setHashtagCheck] = useState("");
+    const [hashTags, setHashTag] = useState([]);
 
-    const {article, isLoading, isError} = useArticle(page, keySearch);
+    const {article, tags, isLoadingTag, isErrorTag, isLoading, isError} = useArticle(page, keySearch);
 
     // const {  data, fetchNextPage } = useInfiniteQuery(
     //     ["article", keySearch],
@@ -69,7 +76,7 @@ const Article = () => {
         }else{
             setKeySearch('');
         }
-        console.log(indexPage);
+
         if(indexPage && typeof indexPage === 'string'){
             setPage(parseInt(indexPage));
         }
@@ -96,15 +103,26 @@ const Article = () => {
         setPage(page++);
     }
 
-    if (isLoading) return <Loading/>;
-    if (isError) return <ErrorPage/>;
+    if (isLoading && isLoadingTag) return <Loading/>;
+    if (isError && isErrorTag) return <ErrorPage/>;
+
+    const hashtagChange = async (hashtagName: string) => {
+        await router.push({
+            pathname: '/article',
+            query: {hashtags: hashtagName}
+        })
+        const newTags = tags.items.filter((tag: any) => tag._id.includes(hashtagName));
+        setHashTag(newTags);
+    }
+
     return (
         <div className={""}>
             <Head>
                 <title>Article Saved</title>
             </Head>
             <div className={"flex flex-col justify-center"}>
-                <div className={"flex justify-center mt-12 mb-10"}>
+
+                <div className={"flex justify-center mt-12 mb-10 flex-col items-center"}>
                     <label htmlFor="default-search"
                            className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
                     <div className="relative w-4/5 md:w-2/5">
@@ -122,8 +140,24 @@ const Article = () => {
                                     className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search
                             </button>
                     </div>
+                    <div className={"flex mt-4"}>
+                        {tags?.items.map((tag: any) => {
+                            return(
+                                <div key={`hashtag-${tag._id}`} onClick={() => hashtagChange(tag._id)}>
+                                    <div key={`hashtag-${tag._id}-field`}
+                                         className={(hashtagCheck === tag._id) ? 'mr-2 mt-3 border bg-green-500 font-extrabold' : 'mr-2 mt-3 text-gray-300 bg-blue-500'}>
+                                        <label
+                                            className={`form-check-label cursor-pointer rounded px-1 py-1 `}
+                                            htmlFor={`hashtag-${tag._id}`}>
+                                            {tag._id}: {tag.count}
+                                        </label>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
                 </div>
-                <ArticleList articleList={article.items}/>
+                <ArticleList articleList={article?.items}/>
                 {/*<p onClick={handlePage}>Load more</p>*/}
             </div>
         </div>
