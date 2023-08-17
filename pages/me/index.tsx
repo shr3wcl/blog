cc2c8af41cd176e0a46a1c2f7e2325747689a566
Tri Pham Minh <minhtri192035@gmail.com>
2023-08-18 04:36:56 +0700

    Update index.tsx

--- pages/me/index.tsx
+++ pages/me/index.tsx
@@ -23,9 +23,13 @@ const Myself = () => {
                     </figcaption>
                 </figure>
 
-                <img className="w-24 h-24 mb-3 mt-5 rounded-full shadow-lg"
+                {/*<img className="w-24 h-24 mb-3 mt-5 rounded-full shadow-lg"
                      src={imgLink}
+                     alt="Avatar"/>*/}
+                <img className="w-24 h-24 mb-3 mt-5 rounded-full shadow-lg"
+                     src={"https://i.pinimg.com/564x/b0/9a/86/b09a86367b4d8fc5a27b8dc2c21393c9.jpg"}
                      alt="Avatar"/>
+                
                 <h5 className="text-blue-500 mb-1 text-xl font-medium dark:text-white">Phạm Minh Trí</h5>
                 <h6 className="text-blue-500 mb-1 text-xl dark:text-white">A.k.a: <b>Bin</b></h6>
                 <span className="text-sm text-gray-500 dark:text-gray-400">Software Engineer - Security Engineer - Penetration Testing</span>
